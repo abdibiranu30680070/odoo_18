089e41c3bcfd9049888f48c42e2d40a0e225e845
abdibiranu30680070 <153322769+abdibiranu30680070@users.noreply.github.com>
2025-11-17 11:55:30 +0300

    Switch to Python3 for PostgreSQL readiness check

--- start-odoo.sh
+++ start-odoo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Wait for PostgreSQL to be ready
-python /usr/local/bin/wait-for-psql.py \
+# Wait for PostgreSQL to be ready using python3
+python3 /usr/local/bin/wait-for-psql.py \
     --db_host=$DATABASE_HOST \
     --db_port=$DATABASE_PORT \
     --db_user=$DATABASE_USER \
